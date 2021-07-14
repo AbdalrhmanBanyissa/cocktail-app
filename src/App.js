73014824fb69524eaf36a1e2edc95ef844858749
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-07-14 12:05:03 +0300

    backend server is connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      home : "",
+    }
+  }
+
+  componentDidMount = () => {
+    const url = process.env.REACT_APP_SERVER_URL
+    axios.get(url).then((homeRec)=>this.setState({home:homeRec.data}))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.home}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
