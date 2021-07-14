70c271df9981807fa8b0749c77bc8bda0cdf443a
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-07-14 18:27:23 +0300

    Navbar is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-
+import axios from "axios";
+import React, { Component } from "react";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Favorite from "./components/Favorite";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      home : "",
-    }
+      home: "",
+    };
   }
 
   componentDidMount = () => {
-    const url = process.env.REACT_APP_SERVER_URL
-    axios.get(url).then((homeRec)=>this.setState({home:homeRec.data}))
-  }
+    const url = process.env.REACT_APP_SERVER_URL;
+    axios.get(url).then((homeRec) => this.setState({ home: homeRec.data }));
+  };
 
   render() {
     return (
       <div>
+        <NavBar />
+        <Router>
+          <Switch>
+            <Route path="/Home">
+              <Home />
+            </Route>
+            <Route path="/Favorite">
+              <Favorite />
+            </Route>
+          </Switch>
+        </Router>
+
         <h1>{this.state.home}</h1>
       </div>
     );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Cocktail App</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/Home">Home</Nav.Link>
+              <Nav.Link href="/Favorite">Favorite</Nav.Link>
+             </Nav>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
